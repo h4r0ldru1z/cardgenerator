3106acf31331d88ed91b9f49af855424af6591af
h4r0ldru1z <haroldand.ruiz@gmail.com>
2022-09-03 03:00:32 +0000

    numero

--- src/app.js
+++ src/app.js
@@ -8,4 +8,9 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let simbolo = ["A", "J", "Q", "K", 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  let random = Math.floor(Math.random() * 13);
+
+  document.querySelector("#numero").append(simbolo[random]);
 };
